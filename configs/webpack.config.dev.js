282fe1cf030fd446d0f5e3c7cd7704179c401ec9
Farhad <faradivar@gmail.com>
2019-12-02 21:04:43 +0330

    some works on dev config

--- configs/webpack.config.dev.js
+++ configs/webpack.config.dev.js
@@ -1,11 +1,16 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
+const rootPath = path.resolve(__dirname, "../");
+const srcPath = path.resolve(rootPath, "src");
+const distPath = path.resolve(rootPath, "dist");
 
 const config = {
     entry: './src/components/index.css',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: distPath,
+        filename: 'bundle.css'
     },
     module: {
         rules: [
@@ -22,7 +27,12 @@ const config = {
         contentBase: path.join(__dirname, '../src'),
         compress: true,
         port: 3000
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(srcPath, "index.html")
+        })
+    ]
 };
 
 module.exports = config;
