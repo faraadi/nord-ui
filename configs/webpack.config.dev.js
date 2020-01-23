28709a800d0f3db1af84ebba7e7515f50fa61d59
Farhad <faradivar@gmail.com>
2020-01-23 12:23:47 +0330

    removed unsued packages

--- configs/webpack.config.dev.js
+++ configs/webpack.config.dev.js
@@ -1,11 +1,10 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
 const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const path = require('path');
+
 const rootPath = path.resolve(__dirname, "../");
 const srcPath = path.resolve(rootPath, "src");
 const distPath = path.resolve(rootPath, "dist");
-const demoPath = path.resolve(rootPath, "demo");
 
 const config = {
     mode: "development",
