d751c70008d96c7f95c388c2fae4f76ec4da2834
Farhad <faradidev@gmail.com>
2020-03-01 11:35:47 +0330

    added install command in home

--- site/src/index.js
+++ site/src/index.js
@@ -1,1 +1,5 @@
 import './index.css';
+const codeSample = document.querySelector("code");
+codeSample.addEventListener("click", function() {
+	window.navigator.clipboard.writeText(codeSample.innerText).then(function(a,b,c) {console.log(a,b,c)})
+})
