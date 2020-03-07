ada4f089f759d7ac747bde7986479197120b6789
Farhad <faradidev@gmail.com>
2020-03-07 12:49:58 +0330

    update docs v0.6.1

--- site/src/assets/documents/js/collapse.script.js
+++ site/src/assets/documents/js/collapse.script.js
@@ -1,6 +1,8 @@
-const button = document.querySelector("[collapse-target]");
+(function() {
+	const button = document.querySelector("[collapse-target]");
 
-button.onclick = function(e) {
-	e.preventDefault();
-	e.target.parentNode.classList.toggle("open")
-}
+	button.onclick = function(e) {
+		e.preventDefault();
+		e.target.parentNode.classList.toggle("open")
+	}
+})();
