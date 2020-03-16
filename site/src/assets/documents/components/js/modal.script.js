95a1b1a3a66f42f234a23ee325daa3028dc6b7f2
Farhad <faradidev@gmail.com>
2020-03-16 14:48:32 +0330

    update docs v0.19.2

--- site/src/assets/documents/components/js/modal.script.js
+++ site/src/assets/documents/components/js/modal.script.js
@@ -1,20 +1,22 @@
-const exampleOneBtn = document.querySelector("#modal-toggler");
-const exampleOneModal = document.querySelector("#modal-example");
+(function() {
+	const exampleOneBtn = document.querySelector("#modal-toggler");
+	const exampleOneModal = document.querySelector("#modal-example");
 
-exampleOneBtn.onclick = function(e) {
-	exampleOneModal.classList.add("open");
-}
-exampleOneModal.onclick = function(e) {
-	exampleOneModal.classList.remove("open");
-}
+	exampleOneBtn.onclick = function(e) {
+		exampleOneModal.classList.add("open");
+	}
+	exampleOneModal.onclick = function(e) {
+		exampleOneModal.classList.remove("open");
+	}
 
-const exampleTwoBtn = document.querySelector("#modal-toggler2");
-const exampleTwoModal = document.querySelector("#modal-example2");
+	const exampleTwoBtn = document.querySelector("#modal-toggler2");
+	const exampleTwoModal = document.querySelector("#modal-example2");
 
-exampleTwoBtn.onclick = function() {
-	exampleTwoModal.classList.add("open");
-}
+	exampleTwoBtn.onclick = function() {
+		exampleTwoModal.classList.add("open");
+	}
 
-exampleTwoModal.onclick = function(e) {
-	if(e.target.isEqualNode(exampleTwoModal)) exampleTwoModal.classList.remove("open");
-}
+	exampleTwoModal.onclick = function(e) {
+		if(e.target.isEqualNode(exampleTwoModal)) exampleTwoModal.classList.remove("open");
+	}	
+})()
