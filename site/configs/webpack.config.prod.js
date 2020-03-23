4b599d6d2d3e2a9f375c8a1ef3b9a70e7e630683
Farhad <faradidev@gmail.com>
2020-03-23 19:54:51 +0430

    update docs v0.23.1

--- site/configs/webpack.config.prod.js
+++ site/configs/webpack.config.prod.js
@@ -33,7 +33,7 @@ module.exports = {
 				case "commons": 
 				case "main": return '[name].[contenthash].js'
 				case "docs": return 'docs/[name].[contenthash].js'
-				case "faq"L return 'faq/[name].[contenthash].js'
+				case "faq": return 'faq/[name].[contenthash].js'
 			}
 		},
 		chunkFilename: '[name].[id].[ext]'
@@ -69,48 +69,48 @@ module.exports = {
 		]
 	},
 	plugins: [
-	new CleanWebpackPlugin(),
-	new MiniCssExtractPlugin({
-		filename: chunkData => {
-			switch (chunkData.chunk.name) {
-				default:
-				case "commons": 
-				case "main": return '[name].[hash].css'
-				case "docs": return 'docs/[name].[hash].css'
-				case "faq": return 'faq/[name].[hash].css'
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: chunkData => {
+				switch (chunkData.chunk.name) {
+					default:
+					case "commons": 
+					case "main": return '[name].[hash].css'
+					case "docs": return 'docs/[name].[hash].css'
+					case "faq": return 'faq/[name].[hash].css'
+				}
+			},
+			chunkFilename: '[id].[hash].css',
+		}),
+		new HtmlWebpackPlugin({
+			template: path.resolve(rootPath, 'src', "index.html"),
+			chunks: ["main", "commons"],
+			templateParameters: {
+				'PUBLIC_URL': process.env.PUBLIC_URL || "/"
 			}
-		},
-		chunkFilename: '[id].[hash].css',
-	}),
-	new HtmlWebpackPlugin({
-		template: path.resolve(rootPath, 'src', "index.html"),
-		chunks: ["main", "commons"],
-		templateParameters: {
-			'PUBLIC_URL': process.env.PUBLIC_URL || "/"
+		}),
+		new HtmlWebpackPlugin({
+			template: path.resolve(rootPath, 'src', "docs/index.html"),
+			chunks: ["docs", "commons"],
+			templateParameters: {
+				'PUBLIC_URL': process.env.PUBLIC_URL || "/docs"
+			},
+			filename: "docs/index.html"
+		}),
+		new HtmlWebpackPlugin({
+			template: path.resolve(rootPath, 'src', "faq/index.html"),
+			chunks: ["faq", "commons"],
+			templateParameters: {
+				'PUBLIC_URL': process.env.PUBLIC_URL || "/faq"
+			},
+			filename: "faq/index.html"
+		}),
+		new DuplicatePackageCheckerPlugin(),
+		new CopyPlugin([
+		{
+			from: publicPath, to: distPath
 		}
-	}),
-	new HtmlWebpackPlugin({
-		template: path.resolve(rootPath, 'src', "docs/index.html"),
-		chunks: ["docs", "commons"],
-		templateParameters: {
-			'PUBLIC_URL': process.env.PUBLIC_URL || "/docs"
-		},
-		filename: "docs/index.html"
-	}),
-	new HtmlWebpackPlugin({
-		template: path.resolve(rootPath, 'src', "faq/index.html"),
-		chunks: ["faq", "commons"],
-		templateParameters: {
-			'PUBLIC_URL': process.env.PUBLIC_URL || "/faq"
-		},
-		filename: "faq/index.html"
-	}),
-	new DuplicatePackageCheckerPlugin(),
-	new CopyPlugin([
-	{
-		from: publicPath, to: distPath
-	}
-	]),
+		]),
 	],
 	devtool: false
 }
